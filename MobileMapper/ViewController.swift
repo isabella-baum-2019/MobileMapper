454cba68d3013f9572b3bd3f8ddff35c306042bc
Isabella Baum 2019 <ibaum9@3012-l25929-s.local>
2019-04-02 09:53:19 -0500

    Zoom Button

--- MobileMapper/ViewController.swift
+++ MobileMapper/ViewController.swift
@@ -7,14 +7,41 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
 
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
+    let locationManager = CLLocationManager()
+    var currentLocation : CLLocation!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        locationManager.requestWhenInUseAuthorization()
+        mapView.showsUserLocation = true
+        
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.startUpdatingLocation()
+    }
+ 
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        currentLocation = locations[0]
+        print(currentLocation)
     }
 
-
+    @IBAction func zoomButtonTapped(_ sender: UIBarButtonItem) {
+        let coordinateSpan = MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
+        let center = currentLocation.coordinate
+        let region = MKCoordinateRegion(center: center, span: coordinateSpan)
+        mapView.setRegion(region, animated: true)
+    }
+    
+    @IBAction func searchButtonTapped(_ sender: UIBarButtonItem) {
+        
+    }
 }
 
